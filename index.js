5ef80013dee2cf6e482f70437c7206bfb5c06633
Jehingson <jehingson25@gmail.com>
2020-08-10 15:29:58 -0430

    Provando Variables de entorno

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+
+const config = require('./config/key')
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
 
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/databaseJon', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -15,5 +26,22 @@ mongoose.connect('mongodb://localhost/databaseJon', {
 
 
 app.get('/', (req, res) => res.send('Hello World'))
+
+app.post('/register', async (req, res) => {
+
+  
+  const user = new User(req.body)
+  console.log(user)
+  const resss = await user.save()
+  
+  if (!resss) {
+    return res.json({success: false})
+  } else {
+    return res.status(200).json({
+      success: true
+    })
+  }
+
+})
 
 app.listen(port, () => console.log(`Server on port: ${port}`))
